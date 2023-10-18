1cbfbe75230c667e9bcef1189f40e3f3081bfa79
Simon Andrieux <simon.andrieux@protonmail.com>
2023-10-18 21:24:59 +0200

    Display 15 running process name to stdout

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::{collections::HashMap, error::Error};
+
+use procfs::process::Process;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let process_by_pid: HashMap<i32, Process> = procfs::process::all_processes()?
+        .into_iter()
+        .map(|process_result| {
+            let process = process_result.unwrap();
+            (process.pid, process)
+        })
+        .collect();
+
+    process_by_pid
+        .into_iter()
+        .take(15)
+        .for_each(|(_pid, process)| match process.stat() {
+            Ok(stat) => println!("{} | {}", process.pid, stat.comm),
+            Err(_) => (),
+        });
+
+    Ok(())
 }
