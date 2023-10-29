dbe9381e6ddd1e0137cb29a8186812e8c4b3aae8
Simon Andrieux <simon.andrieux@protonmail.com>
2023-10-29 19:49:26 +0100

    switch to sk_msg but NULL pointer when trying to attach

--- src/bpf/packet_size.bpf.c
+++ src/bpf/packet_size.bpf.c
@@ -4,8 +4,8 @@
 
 struct
 {
-    __uint(type, BPF_MAP_TYPE_LRU_HASH);
-    __uint(max_entries, 1024);
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __uint(max_entries, 5096);
     __type(key, __u32);
     __type(value, __u64);
 } packet_stats SEC(".maps");
@@ -13,24 +13,26 @@ struct
 char __license[] SEC("license") = "GPL";
 #define TC_ACT_OK 0
 
-SEC("tc")
-int packet_size(struct __sk_buff *ctx)
+SEC("sk_msg")
+int packet_size(struct sk_msg_md *msg)
 {
-    void *data = (void *)(long)ctx->data;
-    void *data_end = (void *)(long)ctx->data_end;
-    int pkt_sz = data_end - data;
+    void *data = (void *)(long)msg->data;
+    void *data_end = (void *)(long)msg->data_end;
+    __u64 pkt_sz = data_end - data;
 
-    int pid = bpf_get_current_pid_tgid() >> 32;
+    __u32 pid = bpf_get_current_pid_tgid() >> 32;
 
-    struct value *value = bpf_map_lookup_elem(&packet_stats, &pid);
+    __u64 *value = bpf_map_lookup_elem(&packet_stats, &pid);
 
     if (value)
     {
-        __sync_fetch_and_add(&value, pkt_sz);
+        *value += pkt_sz;
+        __u64 total_size = (*value) + pkt_sz;
+        bpf_map_update_elem(&packet_stats, &pid, &total_size, BPF_EXIST);
     }
     else
     {
-        bpf_map_update_elem(&packet_stats, &pid, &pkt_sz, BPF_NOEXIST);
+        bpf_map_update_elem(&packet_stats, &pid, &pkt_sz, BPF_ANY);
     }
 
     return TC_ACT_OK;
